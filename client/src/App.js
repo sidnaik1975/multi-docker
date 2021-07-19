2011dc888cf0b7f6a9ae7c9526f99ba248fad843
sidnaik1975 <snaik@equinix.com>
2021-07-18 18:09:34 -0700

    added to cloud111

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Reactxxxx
+            Learn Reactxxxx1
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
